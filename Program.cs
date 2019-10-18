b137eb14690cd607345ebdb5df74adcc44a755a0
ALT-HERIC <mauricedx@gmail.com>
2019-10-18 19:53:01 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaliComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
