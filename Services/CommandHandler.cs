88d219751ea26137fc92fd023219ecc2972a304b
ALT-HERIC <mauricedx@gmail.com>
2019-10-19 16:03:30 +0200

    Added timestamp to log events

--- Services/CommandHandler.cs
+++ Services/CommandHandler.cs
@@ -76,7 +76,7 @@ namespace CaliComp.Services
             // if a command isn't found, log that info to console and exit this method
             if (!command.IsSpecified)
             {
-                System.Console.WriteLine($"Command failed to execute for [{context.User.Username}] : [{result.ErrorReason}]!");
+                System.Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Command failed to execute for {context.User.Username} : {result.ErrorReason}!");
                 return;
             }
                 
@@ -84,13 +84,13 @@ namespace CaliComp.Services
             // log success to the console and exit this method
             if (result.IsSuccess)
             {
-                System.Console.WriteLine($"Command [{command.Value.Name}] executed for: [{context.User.Username}]");
+                System.Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Command {command.Value.Name} executed for: {context.User.Username}");
                 return;
             }
                 
 
             // failure scenario, let's let the user know
-            await context.Channel.SendMessageAsync($"Sorry, {context.User.Username}, something went wrong: [{result}]!");
+            await context.Channel.SendMessageAsync($"Sorry, {context.User.Username}, something went wrong: {result}!");
         }        
     }
 }
